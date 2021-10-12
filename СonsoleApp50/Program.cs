5a9f1905fde5f34311c6a26602e1be5889e951ca
Anton <46803534+devdotnetdotorg@users.noreply.github.com>
2021-10-12 21:24:38 +0300

    added project

---
+++ СonsoleApp50/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace СonsoleApp50
+{    
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test .NET console application!");
+            var str_Framework=RuntimeInformation.FrameworkDescription;
+            var str_OSArch=RuntimeInformation.OSArchitecture.ToString();            
+            var str_OSDesc= RuntimeInformation.OSDescription;  
+            var str_OSIdent=RuntimeInformation.RuntimeIdentifier;          
+            //output
+            Console.WriteLine($"Версия .NET: {str_Framework}");
+            Console.WriteLine($"Архитектура ОС: {str_OSArch}");
+            Console.WriteLine($"Версия ОС: {str_OSDesc}");            
+            Console.WriteLine($"Идентификатор ОС: {str_OSIdent}");
+            //error
+            Console.WriteLine("Генерировать исключение [yes/No]? ");            
+            var input_key=Console.ReadKey().Key;
+            Console.Write("\n");
+            if (input_key==ConsoleKey.Y)
+            {   
+                    Console.Write("Генерация исключения");
+                        throw new Exception("А вот и ошибочка!");
+            }
+             Console.WriteLine("Завершение работы программы");            
+        }
+    }
+}
