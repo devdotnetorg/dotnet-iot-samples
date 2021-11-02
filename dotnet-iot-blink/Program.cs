3e0739d9c07672e5beea7d0df9a41a054ae18565
Anton <46803534+devdotnetdotorg@users.noreply.github.com>
2021-11-02 03:18:08 +0300

    Update

---
+++ dotnet-iot-blink/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Device.Gpio.Drivers;
+using System.Threading;
+
+namespace dotnet_iot_blink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Blink an LED
+            *
+            https://docs.microsoft.com/en-us/dotnet/iot/tutorials/blink-led
+            *
+            */
+            Console.WriteLine("Blinking LED.");
+            //for Linux
+            const int GPIOCHIP = 1;
+            const int LED_PIN = 143; //GPIO143 - bananapi-m64:blue. Board: Banana Pi BPI-M64
+            GpioController controller;
+            var drvGpio = new LibGpiodDriver(GPIOCHIP);
+            controller = new GpioController(PinNumberingScheme.Logical, drvGpio);
+            //
+            controller.OpenPin(LED_PIN, PinMode.Output);
+            bool ledOn = true;
+            while (true)
+            {
+                controller.Write(LED_PIN, ((ledOn) ? PinValue.High : PinValue.Low));
+                Thread.Sleep(1000);
+                ledOn = !ledOn;
+            }
+        }
+    }
+}
---
+++ dotnet-iot-button-from-nanoframework/dotnet-iot-button-from-nanoframework/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Device.Gpio.Drivers;
+using System.Diagnostics;
+using System.Threading;
+using Iot.Device.Button;
+
+namespace dotnet_iot_button_from_nanoframework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Driver Button: Iot.Device.Button
+            sample: https://github.com/nanoframework/nanoFramework.IoT.Device/blob/develop/devices/Button/README.md
+            */
+
+            //for Linux
+            const int GPIOCHIP = 1;
+            const int BUTTON_PIN = 38;
+            GpioController controller;
+            var drvGpio = new LibGpiodDriver(GPIOCHIP);
+            controller = new GpioController(PinNumberingScheme.Logical, drvGpio);
+            
+            // Initialize a new button with the corresponding button pin
+            GpioButton button = new(buttonPin: BUTTON_PIN,controller);
+            
+            Debug.WriteLine("Button is initialized, starting to read state");
+
+            // Enable or disable holding or doublepress events
+            button.IsDoublePressEnabled = true;
+            button.IsHoldingEnabled = true;
+
+            // Write to debug if the button is down
+            button.ButtonDown += (sender, e) =>
+            {
+                Debug.WriteLine($"buttondown IsPressed={button.IsPressed}");
+            };
+
+            // Write to debug if the button is up
+            button.ButtonUp += (sender, e) =>
+            {
+                Debug.WriteLine($"buttonup IsPressed={button.IsPressed}");
+            };
+
+            // Write to debug if the button is pressed
+            button.Press += (sender, e) =>
+            {
+                Debug.WriteLine($"Press");
+            };
+
+            // Write to debug if the button is double pressed
+            button.DoublePress += (sender, e) =>
+            {
+                Debug.WriteLine($"Double press");
+            };
+
+            // Write to debug if the button is held and released
+            button.Holding += (sender, e) =>
+            {
+                switch (e.HoldingState)
+                {
+                    case ButtonHoldingState.Started:
+                        Debug.WriteLine($"Holding Started");
+                        break;
+                    case ButtonHoldingState.Completed:
+                        Debug.WriteLine($"Holding Completed");
+                        break;
+                }
+            };
+
+            //Thread.Sleep(Timeout.Infinite);
+            //Console            
+            string cki;
+            while (true)
+            {
+                Console.Write("Press any key, or 'X' to quit, or ");
+                Console.WriteLine("CTRL+C to interrupt the read operation:");
+                // Start a console read operation. Do not display the input.                
+                cki = Console.In.ReadLineAsync().GetAwaiter().GetResult();
+                // Announce the name of the key that was pressed .
+                Console.WriteLine($"  Key pressed: {cki}\n");
+                // Exit if the user pressed the 'X' key.               
+                if (cki == "X"||cki == "x") break;
+            }
+            //Dispose
+            button.Dispose();
+        }
+    }
+}
