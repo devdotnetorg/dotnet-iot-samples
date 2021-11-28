e9cf7416937a4f2a22b455955d4eff147f17e4f3
Anton <46803534+devdotnetdotorg@users.noreply.github.com>
2021-11-28 19:20:43 +0300

    Added DS18B20 sample

---
+++ dotnet-iot-ds18b20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Iot.Device.OneWire;
+
+namespace dotnet_iot_ds18b20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DS18B20!");
+            try
+            {
+                var devOneWire = OneWireThermometerDevice.EnumerateDevices().FirstOrDefault();
+                double temp=devOneWire.ReadTemperatureAsync().Result.DegreesCelsius;
+                Console.WriteLine($"Temperature = {Math.Round(temp, 2, MidpointRounding.AwayFromZero)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"No 1-Wire sensors found. Exception: {ex.Message}");                
+            }            
+        }
+    }
+}
